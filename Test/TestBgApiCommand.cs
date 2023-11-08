caa4d5cb0b83275a8f52757a0b3fe48848ce3f63
Arsene <tochemey@hotmail.com>
2023-11-08 07:36:56 +0000

    fix: :bug: fix wrong BgApiCommand argument definition (#11)

---
+++ Test/TestBgApiCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DotNetFreeSwitch.Commands;
+using Xunit;
+
+namespace Test
+{
+    public class TestBgApiCommand
+    {
+        [Fact]
+        public void ParseCommand()
+        {
+
+            // create the command
+            var playback = "user/1001 playback:/usr/local/freeswitch/sounds/broadcast/no_prisoner_info.wav,hangup inline";
+            var bgCommand = new BgApiCommand("originate", playback);
+
+            var expected = "bgapi originate user/1001 playback:/usr/local/freeswitch/sounds/broadcast/no_prisoner_info.wav,hangup inline";
+            Assert.Equal(expected, bgCommand.ToString());
+
+        }
+    }
+}
+
